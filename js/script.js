f80169846e1100158637dd1f6ceda7d985ea48ff
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-28 19:48:18 +0000

    js if

--- js/script.js
+++ js/script.js
@@ -18,6 +18,19 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function triangleClick() {
+  
+  const sideA = parseInt(document.getElementById("sideA").value)
+  const sideB = parseInt(document.getElementById("sideB").value)
+  const sideC = parseInt(document.getElementById("sideC").value)
+
+  const angleA = Math.acos((sideB**2 + sideC**2 - sideA**2) / (2 * sideB * sideC)) * (180/Math.PI)
+  const angleB = Math.acos((sideC**2 + sideA**2 - sideB**2) / (2 * sideC * sideA)) * (180/Math.PI)
+  const angleC = Math.acos((sideA**2 + sideB**2 - sideC**2) / (2 * sideA * sideB)) * (180/Math.PI)
+  
+  if (angleA + angleB + angleC == 180) {
+    document.getElementById("triangle-type").innerHTML = "yes"
+  } else {
+    document.getElementById("triangle-type").innerHTML = "no"
+  }
 }
